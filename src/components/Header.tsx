db120be2f30f1de147092656ec2c44c3d80c9acf
RAHULDUSI <106255229+RAHULDUSI@users.noreply.github.com>
2025-09-26 13:25:00 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
 <div className="flex items-center space-x-2">
   <a href="#hero" className="flex items-center">
     <img 
-      src="/GruhaLogo.png" 
+      src="/livgruha_logo_transparent-bjem1q3g.png" 
       alt="Gruha Logo" 
       className="h-10 w-auto"
     />
